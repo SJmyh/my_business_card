6592d7bad4567561058ddc15cd7c5e2f485e1d22
Ivan77web <ivanshestopalov39@gmail.com>
2023-09-24 18:23:55 +0300

    add flex and base navbar

--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom"
 import cl from './Navbar.module.scss';
+import { HStack } from "@/shared/ui/Stack";
 
 export const Navbar = () => {
     return (
-        <div className={cl.navbar}>
-            <Link to={'/'}>Main</Link>
-            <Link to={'/about'}>About</Link>
-        </div>
+        <HStack 
+            justify="between"
+            className={cl.navbar}
+        >
+            <div className={cl.logo}>
+                LOGO
+            </div>
+
+            <div className={cl.links}>
+                <Link to={'/'}>Обо мне</Link>
+                <Link to={'/about'}>Навыки</Link>
+                <Link to={'/about'}>Портфолио</Link>
+                <Link to={'/about'}>Опыт</Link>
+                <Link to={'/about'}>Контакты</Link>
+            </div>
+
+            <div className={cl.logo}>
+                THEME SWITCHER
+            </div>
+        </HStack>
     )
 }
